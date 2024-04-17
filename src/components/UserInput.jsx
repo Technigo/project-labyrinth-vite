e8d8aab70d127fd8b2926b4fea7430f1c60fa5f0
unknown <linda.frischknecht@bluewin.ch>
2024-04-17 12:26:30 +0200

    started on styling

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,5 +1,7 @@
 import { useStartLabyrinthStore } from "../stores/useStartLabyrinthStore";
 
+import "../styles/UserInput.css";
+
 export const UserInput = () => {
   const { userName, setUserName, fetchStart } = useStartLabyrinthStore();
 
@@ -12,17 +14,21 @@ export const UserInput = () => {
   };
 
   return (
-    <>
-      <label>
-        {" "}
-        Choose a very unique Username :)
-        <input
-          type="text"
-          value={userName}
-          onChange={(event) => setUserName(event.target.value)}
-        />
-      </label>
+    <div className="input-page">
+      <label htmlFor="user-input"> Choose a very unique Username :)</label>
+      <input
+        id="user-input"
+        type="text"
+        value={userName}
+        onChange={(event) => {
+          if (event.target.value.length === 0) {
+            alert("Please set the userName");
+          } else {
+            setUserName(event.target.value);
+          }
+        }}
+      />
       <button onClick={handleStartButtonClick}>Start Adventure</button>
-    </>
+    </div>
   );
 };
