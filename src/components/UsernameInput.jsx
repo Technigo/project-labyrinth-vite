07a5d5e2241ea22a2a62ef205fe274bfa1d1428f
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-04-17 12:39:48 +0200

    set up the alarm

--- src/components/UsernameInput.jsx
+++ src/components/UsernameInput.jsx
@@ -5,18 +5,34 @@ import "./UsernameInput.css";
 export const UsernameInput = () => {
   const { setUserName, username } = useGameStore();
 
+  /* const handleUserInput = () => {
+    if (username != "") {
+      addTask(username);
+      setUserName("");
+    } else {
+      alert("Please write a user name");
+    }
+  };*/
+
   return (
     <div>
       <p>{username} </p> New Username:
       <div className="start-user-info">
         <label className="name-label">
           <input
+            type="text"
             className="name-box"
             value={username}
-            onChange={(event) => setUserName(event.target.value)}
+            onChange={(event) => {
+              if (event.target.value.lenght === 0) {
+                alert("Please write a user name");
+              } else {
+                setUserName(event.target.value);
+              }
+            }}
           />
         </label>
-        <GameButton buttonName="Start" />
+        <GameButton /*onClick=(handleUserInput)*/ buttonName="Start" />
       </div>
     </div>
   );
