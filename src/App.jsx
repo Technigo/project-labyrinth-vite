1cfd33fc1521ae3a5a9b4356fa1f6625fc8cc939
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-17 16:15:47 +0200

    fix mistakenly deleted part in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Start } from './components/Start'
-
+import { Start } from './components/Start';
+import { Location } from './components/Location';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
-  return(
-  <>
-  <Start />
-  </>
-  )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/labyrinth' element={<Location />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
