6170ec8f8a16a933430a85b31fef99c755c6b5a0
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-04-18 22:08:52 +0200

    Added backgrounds

--- src/components/ActionPage.jsx
+++ src/components/ActionPage.jsx
@@ -1,44 +1,42 @@
-import { useEffect } from "react";
-
-import { useLabyrinthStore } from "../stores/useLabyrinthStore";
+import { useEffect } from "react"
+import { useLabyrinthStore } from "../stores/useLabyrinthStore"
+import "../style/ActionPage.css";
 
 export const ActionPage = ({ changeBgImg }) => {
   const { coordinates, description, actions, makeMove, restart } =
-    useLabyrinthStore();
+    useLabyrinthStore()
 
   // useEffect that runs on mount of the component and changes the bg img
   useEffect(() => {
-    changeBgImg(coordinates);
-  }, [changeBgImg, coordinates]);
+    changeBgImg(coordinates)
+  }, [changeBgImg, coordinates])
 
   return (
-    <div>
-      ActionPage
-      <p>{coordinates}</p>
+    <div className="action-container">
+      {/* ActionPage
+      <p>{coordinates}</p> */}
       <p>{description}</p>
       {actions.map((action, index) => (
         <button
           value={action.direction}
           key={index}
-          onClick={e => {
-            const direction = e.target.value;
-            console.log(direction);
-            makeMove(direction);
-          }}
-        >
+          onClick={(e) => {
+            const direction = e.target.value
+            console.log(direction)
+            makeMove(direction)
+          }}>
           {action.direction}
         </button>
       ))}
       <button
         type="button"
         onClick={() => {
-          restart();
+          restart()
           // change background back to start page
-          changeBgImg("start");
-        }}
-      >
+          changeBgImg("start")
+        }}>
         Restart
       </button>
     </div>
-  );
-};
+  )
+}
