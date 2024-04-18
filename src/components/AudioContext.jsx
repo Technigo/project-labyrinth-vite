ebed87a03ff8fa002e5a52d970f1250f673b0b3d
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-04-18 16:48:39 +0200

    audio

--- src/components/AudioContext.jsx
+++ src/components/AudioContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react';
 export const AudioContext = createContext();
 
 export const AudioProvider = ({ children }) => {
-  const [audio] = useState(new Audio("/src/audio/ambient-wave-48-tribute-17243.mp3"));
+  const [audio] = useState(new Audio("/public/audio/ambient-wave-48-tribute-17243.mp3"));
   const [isPlaying, setIsPlaying] = useState(false);
 
   const toggleAudio = () => {
