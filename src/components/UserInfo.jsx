027257930440bfd2a8c0c1ddd8a8fb6fc4f07d5c
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-18 08:45:23 +0200

    Fixed issue with input field not working correctly. Removed {handleUserNameChange}.

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,21 +1,23 @@
 import { useEffect, useRef } from "react";
-
 import { useUserStore } from "../stores/useUserStore";
 
 export const UserInfo = () => {
   const { userName, setUserName } = useUserStore();
-  const focusRef = useRef(null);
+  const inputRef = useRef(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Update the global state with the input's current value
+    const newUserName = inputRef.current.value;
+    setUserName(newUserName);
   };
 
-  const handleUserNameChange = (event) => {
-    setUserName(event.target.value);
-  };
+  // const handleUserNameChange = (event) => {
+  //   setUserName(event.target.value);
+  // };
 
   useEffect(() => {
-    focusRef.current.focus();
+    inputRef.current.focus();
   }, []);
 
   return (
@@ -26,10 +28,10 @@ export const UserInfo = () => {
           Enter your username:
           <input
             type="text"
-            ref={focusRef}
-            value={userName}
+            ref={inputRef}
+            defaultValue={userName} // Set initial value to userName, not controlling ongoing input changes
             placeholder="New username"
-            onChange={handleUserNameChange}
+            // onChange={handleUserNameChange}
           />
         </label>
         <button type="submit">Submit</button>
