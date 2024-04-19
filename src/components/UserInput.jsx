a577ccdba1606dba062018f28fbdb102a0168a51
SofieFerrari <sofiestrahl@gmail.com>
2024-04-19 14:59:36 +0200

    trying stuff

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -15,32 +15,34 @@ export const UserInput = () => {
   };
 
   return (
-    <>
-      <div className="background-picture">
-        <Header />
-        <div className="input-page">
-          <h1>
-            Search for the gold plated chest or take a stoll through the forest <br/>- it's up to you!
-          </h1>
-          <label className="start-title" htmlFor="user-input">
-            {" "}
-            Your username:
-          </label>
-          <input
-            className="input-field"
-            id="user-input"
-            type="text"
-            placeholder="chose something rare"
-            value={userName}
-            onChange={(event) => {
-              setUserName(event.target.value);
-            }}
-          />
-          <button className="start-button" onClick={handleStartButtonClick}>
-            Click to start Adventure
-          </button>
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className="background-picture">
+				<img className="background-picture" src="first-page-pic.jpg" />
+				<Header />
+				<div className="input-page">
+					<h1>
+						Search for the gold plated chest or take a stoll through the forest{' '}
+						<br />- let's see where the path takes us!
+					</h1>
+					<label className="start-title" htmlFor="user-input">
+						{' '}
+						Your username:
+					</label>
+					<input
+						className="input-field"
+						id="user-input"
+						type="text"
+						placeholder="chose something rare"
+						value={userName}
+						onChange={(event) => {
+							setUserName(event.target.value)
+						}}
+					/>
+					<button className="start-button" onClick={handleStartButtonClick}>
+						Click to start Adventure
+					</button>
+				</div>
+			</div>
+		</>
+	)
 };
