646bb6bb4a45459eeb0f4fea50e4c582a09b0caa
unknown <linda.frischknecht@bluewin.ch>
2024-04-19 11:39:14 +0200

    adding styling to pages

--- src/components/DisplayLabyrinth.jsx
+++ src/components/DisplayLabyrinth.jsx
@@ -1,56 +1,57 @@
-import { useRef, useEffect } from 'react'
+import Lottie from "lottie-react";
+import { useEffect, useRef } from "react";
 
-import { useLabyrinthStore } from '../stores/useLabyrinthStore'
+import Loading from "../assets/Loading.json";
+import { useLabyrinthStore } from "../stores/useLabyrinthStore";
 
-import Lottie from 'lottie-react'
-import Loading from '../assets/Loading.json'
-
-import '../styles/DisplayLabyrinth.css'
+import "../styles/DisplayLabyrinth.css";
 
 export const DisplayLabyrinth = () => {
-	const backgroundImageRef = useRef()
-	const { loading, start, fetchMove, actions, description, coordinates } =
-		useLabyrinthStore()
+  const backgroundImageRef = useRef();
+  const { loading, start, fetchMove, actions, description, coordinates } =
+    useLabyrinthStore();
 
-    useEffect(() => {
-			const changeBackgroundImage = (coordinates) => {
-				backgroundImageRef.current.style.backgroundImage = `url("${coordinates}.jpg")`
-			}
+  useEffect(() => {
+    const changeBackgroundImage = (coordinates) => {
+      backgroundImageRef.current.style.backgroundImage = `url("${coordinates}.jpg")`;
+    };
 
-			changeBackgroundImage(coordinates)
-		}, [coordinates])
+    changeBackgroundImage(coordinates);
+  }, [coordinates]);
 
-	if (loading) {
-		return  <Lottie animationData={Loading} loop={true} /> 
-	}
+  if (loading) {
+    return <Lottie animationData={Loading} loop={true} />;
+  }
 
-	//Added this part to check if the data is already available for the map function otherwise the code would break.
-	if (!start || !start.actions) {
-		return <div>No data available.</div>
-	}
+  //Added this part to check if the data is already available for the map function otherwise the code would break.
+  if (!start || !start.actions) {
+    return <div>No data available.</div>;
+  }
 
-	console.log(start)
-	console.log(start.actions)
-	console.log(actions)
+  console.log(start);
+  console.log(start.actions);
+  console.log(actions);
 
-	return (
-		<div ref={backgroundImageRef} className="labyrinth-start">
+  return (
+    <div ref={backgroundImageRef} className="labyrinth-start">
       <div className="action-box">
-			<p>{description}</p>
+        <p>{description}</p>
 
-			{actions.map((action) => (
-				<button
-					key={description}
-					value={action.direction}
-					onClick={(e) => {
-						const direction = e.target.value
-						console.log(direction)
-						fetchMove(direction)
-					}}>
-					{action.direction}
-				</button>
-			))}
-    	</div>
-		</div>
-	)
-}
+        {actions.map((action) => (
+          <button
+            className="action-button"
+            key={description}
+            value={action.direction}
+            onClick={(e) => {
+              const direction = e.target.value;
+              console.log(direction);
+              fetchMove(direction);
+            }}
+          >
+            {action.direction}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,38 +1,47 @@
-import { useLabyrinthStore } from '../stores/useLabyrinthStore'
-import { Header } from './Header'
+import { useLabyrinthStore } from "../stores/useLabyrinthStore";
+import { Header } from "./Header";
 
-import '../styles/UserInput.css'
+import "../styles/UserInput.css";
 
 export const UserInput = () => {
-	const { userName, setUserName, fetchStart } = useLabyrinthStore()
+  const { userName, setUserName, fetchStart } = useLabyrinthStore();
 
-	const handleStartButtonClick = () => {
-		if (userName === '') {
-			alert('Please set a username. Thx!')
-		} else {
-			fetchStart(userName)
-		}
-	}
+  const handleStartButtonClick = () => {
+    if (userName === "") {
+      alert("Please set a username. Thx!");
+    } else {
+      fetchStart(userName);
+    }
+  };
 
-	return (
-		<>
-			<div className='background-picture'>
-				<Header />
-				<div className="input-page">
-					<label className="start-title" htmlFor="user-input"> Your username:</label>
-					<input
-						className="input-field"
-						id="user-input"
-						type="text"
-						placeholder="chose something rare"
-						value={userName}
-						onChange={(event) => {
-							setUserName(event.target.value)
-						}}
-					/>
-					<button className="start-button" onClick={handleStartButtonClick}>Click to start Adventure</button>
-				</div>
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div className="background-picture">
+        <Header />
+        <div className="input-page">
+          <h1>
+            Welcome to the path of adventure through the forest of haunted
+            dolls. May the coffee be with you.
+          </h1>
+          <label className="start-title" htmlFor="user-input">
+            {" "}
+            Your username:
+          </label>
+          <input
+            className="input-field"
+            id="user-input"
+            type="text"
+            placeholder="chose something rare"
+            value={userName}
+            onChange={(event) => {
+              setUserName(event.target.value);
+            }}
+          />
+          <button className="start-button" onClick={handleStartButtonClick}>
+            Click to start Adventure
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
