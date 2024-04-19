7c2577f4bf5de4a42516b64c6b94e85c89a9b867
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-04-19 09:12:23 +0200

    lottie

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,11 +1,23 @@
-// import Lottie from "react-lottie";
+import Lottie from 'react-lottie'
+import { animationData } from 'src/lotties/loader.json'
 
 export const Loading = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
   return (
     <section className="content-wrapper">
-      <div className="text-container">
-        <h1 className="paragraph">Loading...</h1>
+      <div>
+        <Lottie options={defaultOptions} height={400} width={400} />
       </div>
+      {/* <div className="text-container">
+        <h1 className="paragraph">Loading...</h1>
+      </div> */}
     </section>
   )
 }
