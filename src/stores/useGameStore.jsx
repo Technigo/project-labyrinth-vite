2c6f01630fedf7b001ef1f832294e7525fb70db5
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-19 20:29:43 +0200

    Fixed buttons. Only display buttons that can be used.

--- src/stores/useGameStore.jsx
+++ src/stores/useGameStore.jsx
@@ -15,7 +15,8 @@ export const useGameStore = create((set) => ({
   setUserName: (newUsername) => set({ username: newUsername }),
   setIsStarted: (newValue) => set({ isStarted: newValue }),
   setLabData: (newLabData) => set({ labData: newLabData }),
-  setIsLoading: (isLoading) => set({ isLoading }),
+  setIsLoading: (newState) => set({ isLoading: newState }),
+
   startGame: (username) => {
     const uniqueName = username + new Date();
     set({ isLoading: true });
@@ -35,31 +36,32 @@ export const useGameStore = create((set) => ({
         console.error("Error:", error);
       });
   },
-  makeMove: (username, action, direction) => {
-    set({ isLoading: true });
-    fetch("https://labyrinth.technigo.io/action", {
-      method: "POST", // or 'PUT'
-      headers: { "Content-Type": "application/json" },
 
-      body: JSON.stringify({
-        username: username,
-        type: action,
-        direction: direction,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-        set({
-          labData: data,
-          isLoading: false,
-          coordinates: data.coordinates,
-          actions: data.actions,
-          description: data.description,
-        });
+  makeMove: (username, action, direction) => {
+    set({ isLoading: true }),
+      fetch("https://labyrinth.technigo.io/action", {
+        method: "POST", // or 'PUT'
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify({
+          username: username,
+          type: action,
+          direction: direction,
+        }),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          set({
+            labData: data,
+            isLoading: false,
+            coordinates: data.coordinates,
+            actions: data.actions,
+            description: data.description,
+          });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
   },
 }));
