5d33bb37f5964ced6ad46e368ebc3bc031bd3223
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-04-20 13:23:59 +0200

    Updating images

--- src/components/UsernameInput.jsx
+++ src/components/UsernameInput.jsx
@@ -4,20 +4,23 @@ import "./UsernameInput.css";
 
 export const UsernameInput = () => {
   const { setUserName, username } = useGameStore();
-  
 
   return (
-    <div>
-      New wanderer:
+    <>
+      <span>New wanderer:</span>
       <div className="start-user-info">
         <label className="name-label">
-          <input type="text" className="name-box" value={username}
+          <input
+            type="text"
+            className="name-box"
+            value={username}
             onChange={(event) => {
-                setUserName(event.target.value);
-                   }}/>
+              setUserName(event.target.value);
+            }}
+          />
         </label>
         <GameButton buttonName="Start" />
       </div>
-    </div>
+    </>
   );
 };
--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -2,19 +2,20 @@ import { UsernameInput } from "./UsernameInput";
 import { GameButton } from "./GameButton";
 import "./WelcomeScreen.css";
 
-
 export const WelcomeScreen = () => {
-return (
+  return (
     <div className="general-container">
       <div className="start-container">
         <h1>The Maze</h1>
-        <p>Enter the labyrinth on your own risk...</p>
-        <p>Can you find a way out of the maze??</p>
+        <p>
+          Enter the labyrinth on your own risk... <br /> Can you find a way out
+          of the maze??
+        </p>
         <div className="start-user">
           <UsernameInput />
           {/*<GameButton buttonName="Start" />*/}
         </div>
       </div>
-      </div>
+    </div>
   );
 };
