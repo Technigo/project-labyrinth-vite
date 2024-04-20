3875be6f52e3ab7e4a4cec3bafef4150068c2d46
Citronskal <yiaswift@gmail.com>
2024-04-20 17:28:56 +0200

    updated Directions

--- src/assets/components/Directions.jsx
+++ src/assets/components/Directions.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { East } from "./buttons/East";
 import { North } from "./buttons/North";
 import { South } from "./buttons/South";
@@ -10,66 +11,44 @@ export const Directions = ({
   coordinates,
   resetGame,
 }) => {
-  console.log("performAction is", typeof performAction);
-
   const startPosition = coordinates === "0,0";
   const endPosition = coordinates === "1,3";
-  console.log(endPosition);
-  console.log(startPosition);
-  console.log(coordinates);
-
-  const onClickRestart = () => {
-    console.log("User clicked restart");
-    resetGame();
-  };
 
   const onClickGo = (type, direction) => {
     console.log(`User clicked ${direction}`);
     performAction(type, direction);
   };
 
+  const directionComponents = useMemo(
+    () => ({
+      South: South,
+      North: North,
+      West: West,
+      East: East,
+    }),
+    []
+  );
+
   return (
     <>
-      {!endPosition &&  (
-        <>
-          {actions.map((action) => {
-            let buttonElement = null;
-            if (action.direction === "South") {
-              buttonElement = (
-                <South
-                  handleClick={() => onClickGo(action.type, "South")}
-                  key={action.direction}
-                />
-              );
-            } else if (action.direction === "North") {
-              buttonElement = (
-                <North
-                  handleClick={() => onClickGo(action.type, "North")}
-                  key={action.direction}
-                />
-              );
-            } else if (action.direction === "West") {
-              buttonElement = (
-                <West
-                  handleClick={() => onClickGo(action.type, "West")}
-                  key={action.direction}
-                />
-              );
-            } else if (action.direction === "East") {
-              buttonElement = (
-                <East
-                  handleClick={() => onClickGo(action.type, "East")}
-                  key={action.direction}
-                />
-              );
-            }
-            return buttonElement;
-          })}
-        </>
-      )}
+      {!endPosition &&
+        actions.map((action) => {
+          const DirectionButton = directionComponents[action.direction];
+          return (
+            DirectionButton && (
+              <DirectionButton
+                handleClick={() => onClickGo(action.type, action.direction)}
+                key={action.direction}
+              />
+            )
+          );
+        })}
       {!startPosition && (
         <MainButton
-          onClick={onClickRestart}
+          onClick={() => {
+            console.log("User clicked restart");
+            resetGame();
+          }}
           style={{ position: "absolute", bottom: "20px", right: "20px" }}
         >
           Restart
@@ -78,33 +57,3 @@ export const Directions = ({
     </>
   );
 };
-// <>
-//   {!endPosition &&
-//     !startPosition &&
-//     actions.map((action) => {
-//       const ButtonComponent =
-//         {
-//           North: North,
-//           South: South,
-//           West: West,
-//           East: East,
-//         }[action.direction] || null;
-
-//       return (
-//         ButtonComponent && (
-//           <ButtonComponent
-//             key={`${action.direction}-${action.type}`}
-//             handleClick={() => onClickGo(action.type, action.direction)}
-//           />
-//         )
-//       );
-//     })}
-//   {!startPosition && (
-//     <MainButton
-//       style={{ position: "absolute", bottom: 20, right: 20 }}
-//       onClick={onClickRestart}
-//     >
-//       Restart
-//     </MainButton>
-//   )}
-// </>
