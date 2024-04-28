316b7caf850bc2d8f7913c5d6baca2576525e80c
Tejpex <equis_angel@hotmail.com>
2024-04-28 15:09:58 +0200

    Added function to start over

--- src/stores/appContentStore.jsx
+++ src/stores/appContentStore.jsx
@@ -14,94 +14,109 @@ export const appContentStore = create((set, get) => ({
   directions: null,
   imageLink: "zerozero",
 
-  toggleLoading: () => set((state) => ({
-    loading: !state.loading
-  })),
-  setGameData: (newData) => set({gameData: newData}),
-  setUserName: (newUserName) => set({userName: newUserName}),
-  setDirections: (array) => set({directions: array}),
+  toggleLoading: () =>
+    set((state) => ({
+      loading: !state.loading,
+    })),
+  setGameData: (newData) => set({ gameData: newData }),
+  setUserName: (newUserName) => set({ userName: newUserName }),
+  setDirections: (array) => set({ directions: array }),
   setImageLink: (coordinates) => {
     switch (coordinates) {
       case "0,0":
-        set({ imageLink: zerozero })
-        break
+        set({ imageLink: zerozero });
+        break;
       case "0,1":
-        set({ imageLink: zeroone })
-        break
+        set({ imageLink: zeroone });
+        break;
       case "0,2":
-        set({ imageLink: zerotwo })
-        break
+        set({ imageLink: zerotwo });
+        break;
       case "0,3":
-        set({ imageLink: zerothree })
+        set({ imageLink: zerothree });
         break;
       case "1,0":
-        set({ imageLink: onezero })
-        break
+        set({ imageLink: onezero });
+        break;
       case "1,1":
-        set({ imageLink: oneone })
-        break
+        set({ imageLink: oneone });
+        break;
       case "1,3":
-        set({ imageLink: onethree })
-        break
+        set({ imageLink: onethree });
+        break;
       default:
-        set({ imageLink: zerozero })
+        set({ imageLink: zerozero });
     }
-  }, 
+  },
   startGame: () => {
-      get().toggleLoading()
-      fetch(`https://labyrinth.technigo.io/start`,{
-            method: "POST",
-            body: JSON.stringify({
-              username: get().userName,
-            }),
-            headers: { "Content-Type": "application/json" },
-        })
+    get().toggleLoading();
+    fetch(`https://labyrinth.technigo.io/start`, {
+      method: "POST",
+      body: JSON.stringify({
+        username: get().userName,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
       .then((response) => response.json())
       .then((json) => {
-        get().setGameData(json)
+        get().setGameData(json);
         if (json.actions.length === 1) {
-        get().setDirections([json.actions[0].direction])
+          get().setDirections([json.actions[0].direction]);
         } else if (json.actions.length === 2) {
-          get().setDirections([json.actions[0].direction, json.actions[1].direction])
+          get().setDirections([
+            json.actions[0].direction,
+            json.actions[1].direction,
+          ]);
         }
-        get().setImageLink(json.coordinates)
+        get().setImageLink(json.coordinates);
       })
       .catch((error) => {
-        console.log("error:", error)
+        console.log("error:", error);
       })
       .finally(
         setTimeout(() => {
-        get().toggleLoading()}, 2000)
-      )
+          get().toggleLoading();
+        }, 2000)
+      );
   },
   continueStory: (direction) => {
-    get().toggleLoading()
+    get().toggleLoading();
     fetch(`https://labyrinth.technigo.io/action`, {
       method: "POST",
       body: JSON.stringify({
         username: get().userName,
         type: "move",
-        direction: direction
+        direction: direction,
       }),
       headers: { "Content-Type": "application/json" },
     })
-    .then((response) => response.json())
-    .then((json) => {
-      get().setGameData(json)
-      if (json.actions.length === 1) {
-        get().setDirections([json.actions[0].direction])
-      } else if (json.actions.length === 2) {
-        get().setDirections([json.actions[0].direction, json.actions[1].direction])
-      }
-      get().setImageLink(json.coordinates)
-    })
-    .catch((error) => {
-      console.log("error:", error)
-    })
-    .finally(
-      setTimeout(() => {
-      get().toggleLoading()}, 2000)
-    )
-  }
-
-}))
+      .then((response) => response.json())
+      .then((json) => {
+        get().setGameData(json);
+        if (json.actions.length === 1) {
+          get().setDirections([json.actions[0].direction]);
+        } else if (json.actions.length === 2) {
+          get().setDirections([
+            json.actions[0].direction,
+            json.actions[1].direction,
+          ]);
+        }
+        get().setImageLink(json.coordinates);
+      })
+      .catch((error) => {
+        console.log("error:", error);
+      })
+      .finally(
+        setTimeout(() => {
+          get().toggleLoading();
+        }, 2000)
+      );
+  },
+  startOver: () =>
+    set({
+      gameData: null,
+      userName: null,
+      directions: null,
+      imageLink: "zerozero",
+  }),
+}));
