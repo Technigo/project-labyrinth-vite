e4f540d0ee3180f69c7a83a2a18ca2e698bac183
Cornelia Dahlin <154337428+lunek1@users.noreply.github.com>
2024-04-29 15:35:35 +0200

    Add files via upload

---
+++ src/components/images/Images.jsx
@@ -0,0 +1,19 @@
+import ArchImage from "../../assets/images/Arch.webp";
+import ArraysImage from "../../assets/images/Arrays.webp";
+import BrightImage from "../../assets/images/Bright.webp";
+import GadgetsImage from "../../assets/images/Gadgets.webp";
+import DoorImage from "../../assets/images/Door.webp";
+import ChestImage from "../../assets/images/Chest.webp";
+import EndImage from "../../assets/images/End.webp";
+import StartImage from "../../assets/images/Start.webp";
+
+export const images = {
+  "0,0": ArchImage,
+  "1,0": ArraysImage,
+  "1,1": BrightImage,
+  "0,1": GadgetsImage,
+  "0,2": DoorImage,
+  "0,3": ChestImage,
+  "1,3": EndImage,
+  start: StartImage,
+};
