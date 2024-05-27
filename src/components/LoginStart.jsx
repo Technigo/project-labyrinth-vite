667bcf5414e5d37a0fc6c5fe11e9486b59d9e7a1
Looziolooz <lorenzo.dastoli@gmail.com>
2024-05-27 23:41:09 +0200

    Fix build issues

--- src/components/LoginStart.jsx
+++ src/components/LoginStart.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useGlobalStoreData } from "../stores/storeData";
+import { useGlobalStoreData } from "../stores/StoreData";
 import "../styles/LoginStart.css";
 
 export const LoginStart = () => {
