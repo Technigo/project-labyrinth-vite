78a67d2b2c61689064780d6cb364eac734a14858
KatSjolin <kat.sjolin@gmail.com>
2024-06-17 09:36:39 +0200

    added more styling and cleaned up code

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useRef } from "react"
-import lottie from "lottie-web"
-import loadingImage from "../assets/animations/loading.json"
-import "./LoadingAnimation.css"
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
+import loadingImage from "../assets/animations/loading.json";
+import "./LoadingAnimation.css";
 
 export const LoadingAnimation = () => {
-  const container = useRef(null)
+  const animationContainer = useRef(null);
 
   useEffect(() => {
     const anim = lottie.loadAnimation({
-      container: container.current,
+      container: animationContainer.current,
       renderer: "svg",
       loop: true,
       autoplay: true,
       animationData: loadingImage,
-    })
+    });
 
     return () => {
       anim.destroy()
-    }
-  }, [])
+    };
+  }, []);
 
   return (
-    <div className="loading-container" style={{ backgroundColor: "black" }}>
-      <div className="loading-animation" ref={container}></div>
+    <div className="loading-container">
+      <div className="loading-animation" ref={animationContainer}></div>
     </div>
-  )
-}
+  );
+};
