0f22541b97de64d60d3d855c55228bc8512f4217
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-06-23 13:00:04 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Directions } from "../components/Directions"
+import { Loading } from "../components/Loading"
+import { Start } from "../components/Start"
 
+// Import all css files
 export const App = () => {
   return (
+    <> 
   <div>
     Labyrinth Project 
   </div>
+
+<Directions/>
+<Loading/>
+<Start/>
+</>
   );
 };
